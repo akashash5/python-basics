60eca73f2fc08a69dccbd5d513b17fe5d7e63ae7
Akash <akash.ash5@gmail.com>
2018-01-24 14:53:10 +0530

    88

--- lists.py
+++ lists.py
@@ -3,4 +3,8 @@ print(city)
 print(city+["Bhopal"])
 for i in reversed(city):
     print(i)
-    
+empty=[]
+letters=['a','b','c','d','e']
+numbers=[1,2,3,4,5]
+mixed=[1,2,"Akash",44]
+print(city[-1])
