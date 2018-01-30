888e18b5cde1dd85a4add09ba918659c80ca04c1
Akash <akash.ash5@gmail.com>
2018-01-30 11:05:52 +0530

    30-1

--- forloops.py
+++ forloops.py
@@ -7,3 +7,13 @@ print(list)
 name="Hi This is Akash"
 for i in name:
     print(i,end=' ')
+
+for n in range(10):
+    if n==6:
+        break
+    print(n,end=',')
+
+for n in range(10):
+    if n%2==0:
+        continue
+    print(n,end=',')
